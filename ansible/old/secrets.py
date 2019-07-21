f61e1f18008895ab2aaa5a421e91fe1d54fe0e64
igefimov <efimov.ig@gmail.com>
2019-07-21 23:32:45 +0200

    delete old

--- ansible/old/secrets.py
+++
@@ -1,2 +0,0 @@
-GCE_PARAMS = ('gcp-service-account@infra-244217.iam.gserviceaccount.com', './infra-244217-6055449b15e9.json')
-GCE_KEYWORD_PARAMS = {'project': 'infra-244217', 'datacenter': 'europe-west1-b'}
