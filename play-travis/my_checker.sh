5dd996f269cc134d08b52feee2fcfffbaf3a677c
igefimov <efimov.ig@gmail.com>
2019-07-21 22:55:27 +0200

    in

--- play-travis/my_checker.sh
+++ play-travis/my_checker.sh
@@ -23,18 +23,18 @@ packer validate -var-file=packer/variables.json.example packer/app.json
 echo '[PACKER] Validate db.json'
 packer validate -var-file=packer/variables.json.example packer/db.json
 
-#ssh-keygen -f ~/.ssh/gcp -q -N ""
-#cd terraform/stage
+ssh-keygen -f ~/.ssh/gcp -q -N ""
+cd terraform/stage
 #cp terraform.tfvars.example terraform.tfvars
-#echo "=============== terraform get ==============="
-#terraform get
-#echo "=============== terraform init ==============="
-#terraform init
-#echo "=============== terraform validate ==============="
-#terraform validate
+echo "=============== terraform get ==============="
+terraform get
+echo "=============== terraform init ==============="
+terraform init
+echo "=============== terraform validate ==============="
+terraform validate
 
 
-echo '=============== start ansible-lint ==============='
+echo '[ANSIBLE] Validate site.yml ==============='
 cd ansible
 ansible-lint playbooks/site.yml --exclude=roles/jdauphant.nginx
 cd ..
