14828ae96cbbb94d8a0eb3f5fa572dae6582b62a
igefimov <efimov.ig@gmail.com>
2019-07-21 22:06:34 +0200

    packer only

--- play-travis/my_checker.sh
+++ play-travis/my_checker.sh
@@ -23,12 +23,12 @@ packer validate -var-file=packer/variables.json.example packer/app.json
 echo '[PACKER] Validate db.json'
 packer validate -var-file=packer/variables.json.example packer/db.json
 
-#ssh-keygen -f ~/.ssh/gcp -q -N ""
-#cd ../terraform/prod
-#cp terraform.tfvars.example terraform.tfvars
-#echo "=============== terraform get ==============="
-#terraform get
-#echo "=============== terraform init ==============="
-#terraform init
-#echo "=============== terraform validate ==============="
-#terraform validate
+ssh-keygen -f ~/.ssh/gcp -q -N ""
+cd ../terraform/prod
+cp terraform.tfvars.example terraform.tfvars
+echo "=============== terraform get ==============="
+terraform get
+echo "=============== terraform init ==============="
+terraform init
+echo "=============== terraform validate ==============="
+terraform validate
